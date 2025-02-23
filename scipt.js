0cbe922c1b4ab40bbef645f713e35300581a8c06
Car Community <steviehoffman2901@icloud.com>
2025-02-23 14:58:44 +0100

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     alert("Willkommen auf der Auto-Community-Seite!");
 });
+function showInfo(car) {
+    let info = {
+        "bmw": "Der BMW M3 ist ein Sportwagen mit hoher Leistung.",
+        "mercedes": "Der Mercedes AMG bietet Luxus und Power in einem.",
+        "audi": "Der Audi RS6 kombiniert Geschwindigkeit mit Eleganz."
+    };
+
+    alert(info[car]);
+}
 
 
