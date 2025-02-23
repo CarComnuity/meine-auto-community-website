5dd64ffd216dd2ccd9446a39cbe800748631d0e1
Car Community <steviehoffman2901@icloud.com>
2025-02-23 16:45:40 +0100

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -12,3 +12,26 @@ function showInfo(car) {
 }
 
 
+function showInfo(car) {
+    let title = "";
+    let text = "";
+
+    if (car === "bmw") {
+        title = "BMW M3";
+        text = "Der BMW M3 ist ein Hochleistungs-Sportwagen mit einem starken Motor.";
+    } else if (car === "mercedes") {
+        title = "Mercedes AMG";
+        text = "Mercedes-AMG bietet leistungsstarke Modelle mit Luxus und Performance.";
+    } else if (car === "audi") {
+        title = "Audi RS6";
+        text = "Der Audi RS6 kombiniert Sportlichkeit mit Alltagstauglichkeit und starkem Motor.";
+    }
+
+    document.getElementById("infoTitle").innerText = title;
+    document.getElementById("infoText").innerText = text;
+    document.getElementById("infoBox").classList.remove("hidden");
+}
+
+function closeInfo() {
+    document.getElementById("infoBox").classList.add("hidden");
+}
